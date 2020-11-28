2e464460ccd2a5d6bef86c9c75d9787185f78075
Madya Ansari <ansarimadya80@gmail.com>
2020-11-28 22:20:31 +0530

    Third commit

--- app/src/main/java/com/example/pharmacystore/Register.java
+++ app/src/main/java/com/example/pharmacystore/Register.java
@@ -3,16 +3,12 @@ package com.example.pharmacystore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button;
+public class Register extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_register);
     }
 }
