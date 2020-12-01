98c8d284d3a32b1a2a8399fab2e838949568c8c5
Madya Ansari <ansarimadya80@gmail.com>
2020-12-01 17:22:30 +0530

    forth commit

--- app/src/main/java/com/example/pharmacystore/MainActivity.java
+++ app/src/main/java/com/example/pharmacystore/MainActivity.java
@@ -3,16 +3,45 @@ package com.example.pharmacystore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView3;
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
     }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
+    public void openActivity3() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/pharmacystore/Register.java
+++ app/src/main/java/com/example/pharmacystore/Register.java
@@ -2,13 +2,31 @@ package com.example.pharmacystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+    private TextView textView10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        textView10 = (TextView) findViewById(R.id.textView10);
+        textView10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
     }
+
+    public void openActivity1() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
