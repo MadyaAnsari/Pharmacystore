ff4f45c2d7d49d8f4477c58126c485d0059f9fe3
Madya Ansari <ansarimadya80@gmail.com>
2020-12-04 23:33:01 +0530

    third commit

--- app/src/main/java/com/example/pharmacystore/Home.java
+++ app/src/main/java/com/example/pharmacystore/Home.java
@@ -9,12 +9,24 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+    private Button loc_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        loc_button = (Button) findViewById(R.id.loc_button);
+        loc_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity4();
+            }
+        });
     }
 
+    public void openActivity4() {
+        Intent intent = new Intent(this, LocationActivity.class);
+        startActivity(intent);
+    }
 }
